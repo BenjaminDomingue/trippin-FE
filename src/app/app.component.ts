798e35f27c1507477f218b5f9a5e5999d8c61e40
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2020-04-16 23:58:30 -0400

    allo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,54 +8,96 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   map: any;
-  theDrawingManager: any;
-  infoWindow: any;
+  // theDrawingManager: any;
+  marker = new google.maps.Marker();
   beginningLat: number;
   beginningLng: number;
+  zoom: number;
+  myLatLng: any;
 
   private geoCoder;
 
   @ViewChild('googlemap', { static: true }) mapView: ElementRef;
 
   ngOnInit() {
-    this.map = this.initializeMap();
+    // this.setCurrentPosition()
+    this.initializeMap();
     // this.theDrawingManager = this.initializeDrawingManager(this.theMap);
   }
 
-  // Initialize the map and get geolocalisation
   initializeMap() {
-    this.map = new google.maps.Map(document.getElementById('googlemap'), {
-      center: { lat: 45.4819173, lng: -73.5756072 },
-      zoom: 6
-    });
-    this.infoWindow = new google.maps.InfoWindow;
-
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        console.log(pos);
-        this.infoWindow.setPosition(pos);
-        this.infoWindow.setContent('Location found.');
-        this.infoWindow.open(this.map);
-        this.map.setCenter(pos);
-      }, function () {
-        handleLocationError(true, this.infoWindow, this.map.getCenter());
+      navigator.geolocation.getCurrentPosition(position => {
+        this.beginningLat = position.coords.latitude;
+        this.beginningLng = position.coords.longitude;
+
+        let myLatLng = new google.maps.LatLng({ lat: this.beginningLat, lng: this.beginningLng });
+
+        let mapProperties = {
+          center: myLatLng,
+          zoom: 14,
+          mapTypeId: 'roadmap'
+        }
+        let map = new google.maps.Map(document.getElementById('googlemap'), mapProperties);
+
+        return map;
       });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, this.infoWindow, this.map.getCenter());
-    }
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-      infoWindow.open(this.map);
     }
   }
+
+  initSearchBox(map) {
+    var input = <HTMLInputElement>document.getElementById('searchInput');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    autocomplete.addListener('place_changed', function () {
+      infowindow.close();
+      marker.setVisible(false);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+      marker.setIcon(({
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(35, 35)
+      }));
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+
+      infowindow.open(map, marker);
+
+    });
+  }
+  // setCurrentPosition() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(position => {
+  //       this.beginningLat = position.coords.latitude;
+  //       this.beginningLng = position.coords.longitude;
+  //       this.zoom = 16;
+  //       this.myLatLng = new google.maps.LatLng({ lat: this.beginningLat, lng: this.beginningLng });
+  //     });
+  //   }
+  // }
 
   // Initialize the drawing manager
   // initializeDrawingManager(map) {
