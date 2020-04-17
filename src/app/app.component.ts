db360e081e712ccde99b67bf4b1f26c962d560f9
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2020-04-17 00:04:55 -0400

    commented code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,49 +45,49 @@ export class AppComponent implements OnInit {
     }
   }
 
-  initSearchBox(map) {
-    var input = <HTMLInputElement>document.getElementById('searchInput');
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // initSearchBox(map) {
+  //   var input = <HTMLInputElement>document.getElementById('searchInput');
+  //   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    autocomplete.bindTo('bounds', map);
+  //   var autocomplete = new google.maps.places.Autocomplete(input);
+  //   autocomplete.bindTo('bounds', map);
 
-    var infowindow = new google.maps.InfoWindow();
-    var marker = new google.maps.Marker({
-      map: map,
-      anchorPoint: new google.maps.Point(0, -29)
-    });
+  //   var infowindow = new google.maps.InfoWindow();
+  //   var marker = new google.maps.Marker({
+  //     map: map,
+  //     anchorPoint: new google.maps.Point(0, -29)
+  //   });
 
-    autocomplete.addListener('place_changed', function () {
-      infowindow.close();
-      marker.setVisible(false);
-      var place = autocomplete.getPlace();
-      if (!place.geometry) {
-        window.alert("Autocomplete's returned place contains no geometry");
-        return;
-      }
+  //   autocomplete.addListener('place_changed', function () {
+  //     infowindow.close();
+  //     marker.setVisible(false);
+  //     var place = autocomplete.getPlace();
+  //     if (!place.geometry) {
+  //       window.alert("Autocomplete's returned place contains no geometry");
+  //       return;
+  //     }
 
-      // If the place has a geometry, then present it on a map.
-      if (place.geometry.viewport) {
-        map.fitBounds(place.geometry.viewport);
-      } else {
-        map.setCenter(place.geometry.location);
-        map.setZoom(17);
-      }
-      marker.setIcon(({
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(35, 35)
-      }));
-      marker.setPosition(place.geometry.location);
-      marker.setVisible(true);
+  //     // If the place has a geometry, then present it on a map.
+  //     if (place.geometry.viewport) {
+  //       map.fitBounds(place.geometry.viewport);
+  //     } else {
+  //       map.setCenter(place.geometry.location);
+  //       map.setZoom(17);
+  //     }
+  //     marker.setIcon(({
+  //       url: place.icon,
+  //       size: new google.maps.Size(71, 71),
+  //       origin: new google.maps.Point(0, 0),
+  //       anchor: new google.maps.Point(17, 34),
+  //       scaledSize: new google.maps.Size(35, 35)
+  //     }));
+  //     marker.setPosition(place.geometry.location);
+  //     marker.setVisible(true);
 
-      infowindow.open(map, marker);
+  //     infowindow.open(map, marker);
 
-    });
-  }
+  //   });
+  // }
   // setCurrentPosition() {
   //   if (navigator.geolocation) {
   //     navigator.geolocation.getCurrentPosition(position => {
