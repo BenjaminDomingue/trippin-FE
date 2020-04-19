134db1c242b29da157044ed1dd1480df4804267d
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2020-04-19 08:21:39 -0400

    sav button created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapDisplayComponent } from './pages/map-display/map-display.component';
+import { SaveButtonComponent } from './components/save-button/save-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapDisplayComponent,
     MapDisplayComponent,
+    SaveButtonComponent,
   ],
   imports: [
     BrowserModule,
