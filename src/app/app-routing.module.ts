96f75fe1bad538f3537da8482bb728b2ada64352
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2020-04-30 08:42:36 -0400

    create navbar and login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './pages/map-display/login-page/login-page.component';
+import { MapDisplayComponent } from './pages/map-display/map-display.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'map', component: MapDisplayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapDisplayComponent } from './pages/map-display/map-display.component';
 import { SaveButtonComponent } from './components/save-button/save-button.component';
+import { NavbarComponent } from './components/save-button/navbar/navbar.component';
+import { LoginPageComponent } from './pages/map-display/login-page/login-page.component';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -12,10 +17,14 @@ import { SaveButtonComponent } from './components/save-button/save-button.compon
     MapDisplayComponent,
     MapDisplayComponent,
     SaveButtonComponent,
+    NavbarComponent,
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
