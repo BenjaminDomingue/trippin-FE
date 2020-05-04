5ee9e508a2df198858e439329d317a9a16112e4d
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2020-05-04 17:02:25 -0400

    moved map into user component

--- src/app/pages/map-display/map-display.component.spec.ts
+++ src/app/pages/map-display/map-display.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MapDisplayComponent } from './map-display.component';
+// import { MapDisplayComponent } from './map-display.component';
 
-describe('MapDisplayComponent', () => {
-  let component: MapDisplayComponent;
-  let fixture: ComponentFixture<MapDisplayComponent>;
+// describe('MapDisplayComponent', () => {
+//   let component: MapDisplayComponent;
+//   let fixture: ComponentFixture<MapDisplayComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MapDisplayComponent ]
-    })
-    .compileComponents();
-  }));
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ MapDisplayComponent ]
+//     })
+//     .compileComponents();
+//   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MapDisplayComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(MapDisplayComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
