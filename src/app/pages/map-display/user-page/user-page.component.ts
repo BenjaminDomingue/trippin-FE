444cdffce0212c4444ac90d339fac1207da97cfa
Benjamin Domingue <benjamindomingue@Benjamins-MacBook-Air.local>
2020-05-10 21:43:27 -0400

    added buttons in show page

--- src/app/pages/map-display/user-page/user-page.component.ts
+++ src/app/pages/map-display/user-page/user-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { } from 'googlemaps';
 import { ItineraryService } from 'src/app/services/itinerary.service';
 import { Itinerary } from 'src/app/models/itinerary.model';
@@ -8,13 +8,29 @@ import { Itinerary } from 'src/app/models/itinerary.model';
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.css']
 })
-export class UserPageComponent {
+export class UserPageComponent implements OnInit{
+  showButtons: boolean | undefined;
+  showMap: boolean | undefined;
 
   constructor(private readonly itineraryService: ItineraryService) { }
+
+  ngOnInit() {
+    this.showButtons = true;
+    this.showMap = false;
+  }
 
   saveItinerary(itinerary: Itinerary) {
     this.itineraryService.saveItinerary(itinerary).subscribe(response => {
       return response;
     });
+  }
+
+  createItinerary() {
+    this.showMap = true;
+    this.showButtons = false;
+  }
+
+  lookItineraries() {
+    this.showButtons = false;
   }
 }
