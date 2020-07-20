bac60edb1708b8ab3d48c96f8ade2747a9ea7e55
Benjamin Domingue <benjamin.domingue@beslogic.com>
2020-07-19 23:11:47 -0400

    removed unnecessary form in select menu

--- src/app/components/select-menu/select-menu.component.ts
+++ src/app/components/select-menu/select-menu.component.ts
@@ -1,13 +1,5 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  PipeTransform,
-  Output,
-  EventEmitter,
-} from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { TravelMode } from "src/app/models/travelModeEnum.mode";
-import { FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: "app-select-menu",
@@ -18,30 +10,16 @@ export class SelectMenuComponent implements OnInit {
   travelModeArray = Object.keys(TravelMode).map(
     (key) => TravelMode[key as any]
   );
-  dropdownForm: FormGroup;
 
   @Output()
   sendSelectedTravelMode = new EventEmitter<string>();
 
   ngOnInit() {
-    this.createForm();
     this.travelModeArray;
   }
-
-  constructor(private fb: FormBuilder) {}
-
-  createForm = () => {
-    this.dropdownForm = this.fb.group({
-      selectedTravelMode: [""],
-    });
-  };
 
   selectChangeHandler(event: any) {
     // this.selectedTravelMode.value = event.target.value;
     this.sendSelectedTravelMode.emit(event.target.value);
-  }
-
-  get selectedTravelMode() {
-    return this.dropdownForm.get("dropdown");
   }
 }
